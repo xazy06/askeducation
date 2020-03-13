db29495fc31fd2bdbf3cd1d27f3e45fc6f50acd6
arthur33277 <xazy06@gmail.com>
2020-03-13 03:54:17 +0300

    https://github.com/xazy06/askeducation/issues/18

--- client/src/views/components/bfooter/index.vue
+++ client/src/views/components/bfooter/index.vue
@@ -7,15 +7,17 @@
         а также по телефону или по Skype:
       </div>
       <div class="s-mb_20">
-        <a class="s-accent" href="">+7 (495) 730-20-82</a>
+        <a class="s-accent" href="tel:+74993222316">+7 (499) 322-23-16</a>
       </div>
       <div class="s-mb_10">
         Мы работаем с понедельника по пятницу
       </div>
-      <div class="s-accent s-mb_20">с 9:00 до 19:00</div>
+      <div class="s-accent s-mb_20">с 10:00 до 19:00</div>
       <div>
         <img class="s-middle" src="@/assets/img/map.png"/>
-        <span class="s-middle">Москва, Костомаровский переулок, дом 3, строение 1a, офис 313</span>
+        <a class="s-accent" target="_blank" href="https://yandex.ru/maps/213/moscow/?ll=37.622920%2C55.701085&mode=routes&rtext=~55.701166%2C37.622792&rtt=auto&ruri=~ymapsbm1%3A%2F%2Fgeo%3Fll%3D37.623%252C55.701%26spn%3D0.001%252C0.001%26text%3D%25D0%25A0%25D0%25BE%25D1%2581%25D1%2581%25D0%25B8%25D1%258F%252C%2520%25D0%259C%25D0%25BE%25D1%2581%25D0%25BA%25D0%25B2%25D0%25B0%252C%2520%25D0%2592%25D0%25B0%25D1%2580%25D1%2588%25D0%25B0%25D0%25B2%25D1%2581%25D0%25BA%25D0%25BE%25D0%25B5%2520%25D1%2588%25D0%25BE%25D1%2581%25D1%2581%25D0%25B5%252C%25205&z=19">
+          <span class="s-middle">Москва, Варшавское шоссе, д.5, офис 30 (2 этаж).</span>
+        </a>
       </div>
     </div>
   </div>
@@ -61,7 +63,7 @@ export default {
 
   .b-footer-in::before {
     content: "";
-    background: url(zabor.png) 0 0 repeat-x;
+    /*background: url(zabor.png) 0 0 repeat-x;*/
     display: block;
     height: 14px;
     width: 100%;
