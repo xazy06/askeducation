d7b284a5044301be79a851efad9047e0c58e2a75
arthur33277 <xazy06@gmail.com>
2020-03-25 05:58:51 +0300

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,45 @@
 #!/usr/bin/env bash
 
-sudo rm -R *
+# 1) remote
+# --------------------------------------------
+# ssh root@80.78.240.22
+# iph3ohhoo7un
 
-#npm install pm2 -g && pm2 update
+# cd askeducation
+# pm2 stop ecosystem.config.js --env production
+# sudo rm -R *
 
-git clone https://github.com/xazy06/askeducation.git && cd askeducation
+# 2) local
+# ---------------------------------------------
+# npm run build && cd client && npm run build  && cd ../ && pscp.exe -r ./dist root@80.78.240.22:/root/askeducation
+# iph3ohhoo7un
 
-git checkout staging && git pull && npm run build && pm2 start ecosystem.config.js --env production
+# 3)
+# --------------------------------------------
+# ssh root@80.78.240.22
+# iph3ohhoo7un
 
-
-
-
-# before it build clent
-# pscp.exe -r ./dist root@80.78.240.22:/root/askeducation
-
+# cd askeducation
 # pm2 start ecosystem.config.js --env production
 
-# chmod +x ./deploy.sh
+# end
 
+
+
+
+
+
+
+
+
+
+
+# sudo rm -R *
+# npm install pm2 -g && pm2 update
+# git clone https://github.com/xazy06/askeducation.git && cd askeducation
+# git checkout staging && git pull && npm run build && pm2 start ecosystem.config.js --env production
+# before it build clent
+# pscp.exe -r ./dist root@80.78.240.22:/root/askeducation
+# pm2 start ecosystem.config.js --env production
+# chmod +x ./deploy.sh
 #sudo rm -R .git
